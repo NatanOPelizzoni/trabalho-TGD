4d98cfcdfb4e12839a51cfa841b6ae7c55db19cb
Natan O Pelizzoni <natan2624@gmail.com>
2022-07-01 02:53:27 -0300

    Criação do layout das telas aluno, livro e locacao

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,28 +3,92 @@ const route = express.Router();
 
 const services = require('../services/render');
 
-/**
- *  @description Root Route
- *  @method GET /
- */
-route.get('/', services.homeRoutes);
+//Rota Index
 
 /**
- *  @description autor
- *  @method GET /autor
- */
+*  @description Root Route
+*  @method GET /
+*/
+route.get('/', services.index);
+
+//Rotas Autor
+
+/**
+*  @description autor
+*  @method GET /autor
+*/
 route.get('/autor', services.autor);
 
 /**
- *  @description add autores
- *  @method GET /add_autor
- */
+*  @description add autor
+*  @method GET /add_autor
+*/
 route.get('/add_autor', services.add_autor);
 
 /**
- *  @description for update_autor
- *  @method GET /update_autor
- */
+*  @description for update_autor
+*  @method GET /update_autor
+*/
 route.get('/update_autor', services.update_autor);
+
+//Rotas Aluno
+
+/**
+*  @description aluno
+*  @method GET /aluno
+*/
+route.get('/aluno', services.aluno);
+
+/**
+*  @description add aluno
+*  @method GET /add_aluno
+*/
+route.get('/add_aluno', services.add_aluno);
+ 
+/**
+*  @description for update_aluno
+*  @method GET /update_aluno
+*/
+route.get('/update_aluno', services.update_aluno);
+
+//Rotas Livro
+
+/**
+*  @description livro
+*  @method GET /livro
+*/
+route.get('/livro', services.livro);
+
+/**
+*  @description add livro
+*  @method GET /add_livro
+*/
+route.get('/add_livro', services.add_livro);
+ 
+/**
+*  @description for update_livro
+*  @method GET /update_livro
+*/
+route.get('/update_livro', services.update_livro);
+
+//Rotas Locação
+
+/**
+*  @description locacao
+*  @method GET /locacao
+*/
+route.get('/locacao', services.locacao);
+
+/**
+*  @description add locacao
+*  @method GET /add_locacao
+*/
+route.get('/add_locacao', services.add_locacao);
+ 
+/**
+*  @description for update_locacao
+*  @method GET /update_locacao
+*/
+route.get('/update_locacao', services.update_locacao);
 
 module.exports = route
--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,9 @@
 
-exports.homeRoutes = function(req, res){
+exports.index = function(req, res){
     res.render('index');
 }
+
+//Exports Autor
 
 exports.autor = function(req, res){
     res.render('autor');
@@ -13,4 +15,46 @@ exports.add_autor = function(req, res){
 
 exports.update_autor = function(req, res){
     res.render('update_autor');
+}
+
+//Exports Aluno
+
+exports.aluno = function(req, res){
+    res.render('aluno');
+}
+
+exports.add_aluno = function(req, res){
+    res.render('add_aluno');
+}
+
+exports.update_aluno = function(req, res){
+    res.render('update_aluno');
+}
+
+//Exports Livro
+
+exports.livro = function(req, res){
+    res.render('livro');
+}
+
+exports.add_livro = function(req, res){
+    res.render('add_livro');
+}
+
+exports.update_livro = function(req, res){
+    res.render('update_livro');
+}
+
+//Exports Locação
+
+exports.locacao = function(req, res){
+    res.render('locacao');
+}
+
+exports.add_locacao = function(req, res){
+    res.render('add_locacao');
+}
+
+exports.update_locacao = function(req, res){
+    res.render('update_locacao');
 }
